e52cebb858b1d47d0a2d6d6a0f97f3fdfd4fe0b4
subtodempen <pehlivandemirel@yahoo.com>
2025-08-03 21:13:45 +0100

    Added error handler to Crawler._getHtml and wrote tests for teh function.

--- src/Scraper/RightMove/page_indexer.py
+++ src/Scraper/RightMove/page_indexer.py
@@ -1,15 +1,46 @@
 from bs4 import BeautifulSoup
+
 import requests
+from requests.adapters import HTTPAdapter, Retry
+
+import logging
+
 
 class Crawler:
     def __init__(self):
+        self.crawledUrls = []   
         self.currPage = None
-        self.crawledUrls = []    
-    
-    def _getHtml(self, url):
-        response = requests.get(url)
-        return response.text
 
+    def _getHtml(self, url):
+        session = requests.Session()
+        
+        # construct a request tiemout loop and try 5 more 
+        MAX_RETRY = 5
+        BACK_OFF = 1
+        ERROR_CODES = [500, 502, 504]
+
+        retries = Retry(
+            total=MAX_RETRY, 
+            backoff_factor=BACK_OFF,
+            status_forcelist=ERROR_CODES
+        )
+            
+
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+
+        try:
+            response = session.get(url)
+
+        except requests.exceptions.TooManyRedirects:
+            logging.warning("invalid URL, base crawler URL is invalid")
+            return None
+
+        except requests.exceptions.RequestException as e:
+            logging.error("Catostrophic request error" + e)
+            return None
+        
+        return response.text
            
     def _initSoupEngine(self, htmlDoc):
         return BeautifulSoup(htmlDoc)
@@ -36,14 +67,13 @@ class Crawler:
         index = pageUrl.find(indentifier)
 
         if index == -1:
-            return pageUrl + indentifier
+            secondPageIndex = '2'
+            return pageUrl + indentifier + secondPageIndex # if no ?pageNumber index then go to second page
         
-        for i in range(index, index + len(indentifier)):
-            prevPageNumber += pageUrl[i]
-
-        pageNumber = int(prevPageNumber) + 1
-        return pageUrl.replace(prevPageNumber, str(pageNumber))
-
+        prevPageNumber = pageUrl[index + len(indentifier)]
+    
+        nextPageNumber = int(prevPageNumber) + 1
+        return pageUrl.replace(prevPageNumber, str(nextPageNumber))
 
     
     def crawlPages(self):
