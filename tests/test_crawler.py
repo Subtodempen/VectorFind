558c384bf4167d49c014ed72f02aece91f2413e1
subtodempen <pehlivandemirel@yahoo.com>
2025-08-04 21:06:17 +0100

    Finished and passed all unit tests for web crawler

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -3,13 +3,18 @@ import pytest
 from bs4 import BeautifulSoup
 from src.Scraper.RightMove.page_indexer import Crawler
 
+import json
 
 @pytest.fixture
 def crawlerWrapper():
-    crawler = Crawler()
+    crawler = Crawler(None)
     crawler.currPage = "https://www.rightmove.co.uk/house-prices/london.html"
 
     return crawler 
+
+@pytest.fixture
+def crawlerStateWrapper():
+    return Crawler("test.json")
 
 def testLinkRetrieval(crawlerWrapper):
     expectedResults = [
@@ -36,10 +41,47 @@ def testPageIncrementer(crawlerWrapper):
     assert crawlerWrapper._incrementPageUrl(startUrl) == nextUrl
     assert crawlerWrapper._incrementPageUrl(fringeCaseUrl) == nextUrl
 
-def testStateLoad():
-    pass
+def testHtmlGetter(crawlerWrapper):
+    testUrl = "https://httpbin.org/html"
+    expectedResult = "Herman Melville - Moby-Dick"
 
-def testStateSave():
-    pass
+    actualResults = crawlerWrapper._getHtml(testUrl)
+
+    assert expectedResult in actualResults
+    
+    
+def testStateSave(crawlerStateWrapper):
+    expectedResult = "SAVE TEST URL"
+    testFile = crawlerStateWrapper.JSONHandler.jsonFName
+        
+    crawlerStateWrapper.currPage = expectedResult
+    crawlerStateWrapper.crawledPages = []
+    
+    crawlerStateWrapper.JSONHandler.saveCrawlState()
+
+    # open json file and try and find the expectedResul at currUrl
+    with open(testFile, mode="r", encoding="utf-8") as jsonFile:
+        rawJson = json.load(jsonFile)
+
+        assert rawJson["currPage"] == expectedResult
 
 
+def testStateLoad(crawlerStateWrapper):
+    expectedResult = "LOAD TEST URL" 
+    testFile = crawlerStateWrapper.JSONHandler.jsonFName
+    
+    crawlerStateWrapper.currPage = expectedResult
+    crawlerStateWrapper.crawledPages = []
+
+    # save the classes state
+    crawlerStateWrapper.JSONHandler.saveCrawlState()
+    
+    # reset the classes state
+    crawlerStateWrapper.currPage = None
+    
+    # load the classes state
+    crawlerStateWrapper.JSONHandler.loadCrawlState()
+    
+    # Asserting that the proccess was succseful
+    assert crawlerStateWrapper.currPage == expectedResult
+    
