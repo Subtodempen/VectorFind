b0036fdedcf5b0e9d18528c5e12f04c4fb45956a
subtodempen <pehlivandemirel@yahoo.com>
2025-08-09 23:44:26 +0100

    Cleaned out main function

--- src/main.py
+++ src/main.py
@@ -4,36 +4,6 @@ from postgres import postgresWrapper
 import torch
 import argparse
 
-def initCLIPWrapper():
-    clip = CLIPWrapper("openai/clip-vit-base-patch32")
-    clip.initCLIP()
-    
-    return clip
-
-def initDBWrapper():
-    db = postgresWrapper("vecquery")   
-    db.loadConfig('../database.ini')   
-    db.connectToDatabase()
-
-    return db    
-
-def processAndEmbedImage(CLIPWrapper, path):
-    image = CLIPWrapper.loadImage(path)
-    vectorImg = CLIPWrapper.processImage(image)    
-    embeddedImg = CLIPWrapper.embedImage(vectorImg)
-
-    return embeddedImg
-
-def convertTorchToNumPy(tensor):
-    tensor32 = tensor.to(torch.float32)
-    return tensor32.numpy().tolist()[0]
-
-def insertAddressVec(dbWrapper, imageVec, address):
-    dbWrapper.bufferedAppend(imageVec, address)
-    dbWrapper.flushBuffer()
-
-    dbWrapper.commitTransaction()
-
 
 if __name__ == "__main__":
     CLIPWrapper = initCLIPWrapper()
